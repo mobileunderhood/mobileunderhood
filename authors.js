e8fae2b178cdf010b4619df90666a228a2af86c1
Agapov Alexey <agapov.one@gmail.com>
2020-06-14 22:11:48 +0500

    Update authors.js

--- authors.js
+++ authors.js
@@ -1,12 +1,12 @@
 import authorId from './helpers/author-id';
 
 export default authorId([
-    { username: 'anioutkajarkova', first: '1269842596546973697', post: false },
   // {
   //   username: '' // twitter name of author. if author has no twitter - use underhood name
   //   first: '',  # id of first tweet
   //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
   // },
+  { username: 'anioutkajarkova', first: '1269842596546973697' },
   { username: 'ashumidub', first: '1267306498780278785' },
   { username: 'RetainCycle', first: '1264821218424885248' },
   { username: 'Arturstwit', first: '1262234998221856770' },
