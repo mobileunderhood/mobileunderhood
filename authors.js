55313cdbe592b0454143e06e9de00e7b6b5d4b62
anybot <anybot@anyunderhood>
2020-08-10 07:06:48 +0500

    Update authors.js

--- authors.js
+++ authors.js
@@ -6,6 +6,7 @@ export default authorId([
   //   first: '',  # id of first tweet
   //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
   // },
+  { username: 'day_spring', first: '1290142501127335937' },
   { username: 'srggrch', first: '1287622520351449088' },
   { username: 'aydarmukh', first: '1285070629935501312' },
   { username: 'krossovochkin', first: '1282530164019265536' },
