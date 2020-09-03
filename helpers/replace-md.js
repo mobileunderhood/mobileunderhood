3e6ce013ec5f8140989a10d0dd306fbbb9a6a4c3
Alexey Agapov <agapov.one@gmail.com>
2020-09-03 23:48:02 +0500

    Add instructions on anybot

--- helpers/replace-md.js
+++ helpers/replace-md.js
@@ -1,8 +1,10 @@
-import { underhood, curator } from '../underhood'
+import { underhood, github, curator } from '../underhood';
 
 export default function (str) {
   return str
     .replace(/{{underhood.name}}/g, underhood.name)
     .replace(/{{underhood.description}}/g, underhood.description)
-    .replace(/{{curator.twitter}}/g, curator.twitter)
+    .replace(/{{github.user}}/g, github.user)
+    .replace(/{{github.repo}}/g, github.repo)
+    .replace(/{{curator.twitter}}/g, curator.twitter);
 }
